8c94bd3ec980d5942a34d4c50d26c8e564dc9099
Thomas Gray <tgray017@gmail.com>
2019-04-29 13:45:36 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-user1 = User.new(username: "tgray", password: "password", balance: 100)
-user2 = User.new(username: "victoria", password: "password", balance: 50)
+user1 = User.create(username: "tgray", password: "password", balance: 100)
+user2 = User.create(username: "victoria", password: "password", balance: 50)
